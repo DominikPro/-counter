d6a7139b75a929f54c9597084992b10c3ed21715
Dominik Proficz <dominik.proficz@gmail.com>
2019-05-02 12:54:07 +0200

    Próbowałem dodać drugo counter

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let czasnaPrzewiniecie = 10; //W mili sekundach szybkość przewijania
 let wielkosc = 66; // Do jakiek liczby ma przewijać
 let number = 0; // liczba startowa
 let interwal;
+
 let spanNumber = document.getElementById("number");
 interwal = setInterval(changeNmber, czasnaPrzewiniecie);
 function changeNmber() {
