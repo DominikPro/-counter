db222bc22e634a20bb72e6f3e71074456d5eafa7
Dominik Proficz <dominik.proficz@gmail.com>
2019-05-02 12:09:37 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+let czasnaPrzewiniecie = 10; //W mili sekundach
+let wielkosc = 66; // Do jakiek liczby ma przewijać
+let number = 0; // liczba startowa
+let interwal;
+let spanNumber = document.getElementById("number");
+interwal = setInterval(changeNmber, czasnaPrzewiniecie);
+function changeNmber() {
+  if (number < wielkosc) {
+    number++;
+  }
+  spanNumber.innerHTML = number;
+}
+changeNmber();
