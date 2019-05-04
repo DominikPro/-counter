5c22807981cae32ebb3419a47150ddce67fbcb37
Dominik Proficz <dominik.proficz@gmail.com>
2019-05-04 12:32:35 +0200

    added animation appearing countdown

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-let czasnaPrzewiniecie = 10; //W mili sekundach szybkość przewijania
+let czasnaPrzewiniecie = 28; //W mili sekundach szybkość przewijania
 let wielkosc = 66; // Do jakiek liczby ma przewijać
 let number = 0; // liczba startowa
 let interwal;
 
 let spanNumber = document.getElementById("number");
 interwal = setInterval(changeNmber, czasnaPrzewiniecie);
+
 function changeNmber() {
   if (number < wielkosc) {
     number++;
